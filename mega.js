d94a7b60083e4c81b3e8aa063e74db96f853efff
afnanjanooskan <140999326+afnanjanooskan@users.noreply.github.com>
2026-05-27 23:55:34 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-  email: "Enter your mega email",
-  password: "Enter your mega password",
+  email: "footb3075@gmail.com",
+  password: "Afnan9090##",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
