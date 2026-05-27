3dc297895decb42002644ddddd690ff6ff4d6c19
afnanjanooskan <140999326+afnanjanooskan@users.noreply.github.com>
2026-05-28 00:59:27 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const fs = require("fs");
 const { exec } = require("child_process");
+
 let router = express.Router();
+
 const pino = require("pino");
+
 const {
   default: makeWASocket,
   useMultiFileAuthState,
@@ -11,6 +14,7 @@ const {
   Browsers,
   jidNormalizedUser,
 } = require("@whiskeysockets/baileys");
+
 const { upload } = require("./mega");
 
 function removeFile(FilePath) {
@@ -20,8 +24,10 @@ function removeFile(FilePath) {
 
 router.get("/", async (req, res) => {
   let num = req.query.number;
+
   async function RobinPair() {
-    const { state, saveCreds } = await useMultiFileAuthState(`./session`);
+    const { state, saveCreds } = await useMultiFileAuthState("./session");
+
     try {
       let RobinPairWeb = makeWASocket({
         auth: {
@@ -31,43 +37,66 @@ router.get("/", async (req, res) => {
             pino({ level: "fatal" }).child({ level: "fatal" })
           ),
         },
+
         printQRInTerminal: false,
+
         logger: pino({ level: "fatal" }).child({ level: "fatal" }),
+
         browser: Browsers.macOS("Safari"),
       });
 
       if (!RobinPairWeb.authState.creds.registered) {
         await delay(1500);
+
         num = num.replace(/[^0-9]/g, "");
+
         const code = await RobinPairWeb.requestPairingCode(num);
+
         if (!res.headersSent) {
           await res.send({ code });
         }
       }
 
       RobinPairWeb.ev.on("creds.update", saveCreds);
+
       RobinPairWeb.ev.on("connection.update", async (s) => {
         const { connection, lastDisconnect } = s;
+
         if (connection === "open") {
           try {
             await delay(10000);
-            const sessionPrabath = fs.readFileSync("./session/creds.json");
+
+            const sessionPrabath = fs.readFileSync(
+              "./session/creds.json"
+            );
 
             const auth_path = "./session/";
-            const user_jid = jidNormalizedUser(RobinPairWeb.user.id);
 
-            function randomMegaId(length = 6, numberLength = 4) {
+            const user_jid = jidNormalizedUser(
+              RobinPairWeb.user.id
+            );
+
+            function randomMegaId(
+              length = 6,
+              numberLength = 4
+            ) {
               const characters =
                 "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
               let result = "";
+
               for (let i = 0; i < length; i++) {
                 result += characters.charAt(
-                  Math.floor(Math.random() * characters.length)
+                  Math.floor(
+                    Math.random() * characters.length
+                  )
                 );
               }
+
               const number = Math.floor(
                 Math.random() * Math.pow(10, numberLength)
               );
+
               return `${result}${number}`;
             }
 
@@ -81,25 +110,49 @@ router.get("/", async (req, res) => {
               ""
             );
 
-            const sid = `*Everything is Afnan*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*wa.me/message/WKGLBR2PCETWD1*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/GAOhr0qNK7KEvJwbenGivZ*`;
-            const mg = `🛑*Don't share this code to anyone*🛑`;
-            const dt = await RobinPairWeb.sendMessage(user_jid, {
-              image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
-              },
-              caption: sid,
-            });
-            const msg = await RobinPairWeb.sendMessage(user_jid, {
-              text: string_session,
-            });
-            const msg1 = await RobinPairWeb.sendMessage(user_jid, { text: mg });
+            const sid =
+              `*Everything is Afnan*\n\n` +
+              `👉 ${string_session} 👈\n\n` +
+              `*This is the your Session ID, copy this id and paste into config.js file*`;
+
+            const mg =
+              `🛑*Don't share this code to anyone*🛑`;
+
+            const dt = await RobinPairWeb.sendMessage(
+              user_jid,
+              {
+                image: {
+                  url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                },
+
+                caption: sid,
+              }
+            );
+
+            const msg = await RobinPairWeb.sendMessage(
+              user_jid,
+              {
+                text: string_session,
+              }
+            );
+
+            const msg1 = await RobinPairWeb.sendMessage(
+              user_jid,
+              {
+                text: mg,
+              }
+            );
+
           } catch (e) {
             exec("pm2 restart prabath");
           }
 
           await delay(100);
+
           return await removeFile("./session");
+
           process.exit(0);
+
         } else if (
           connection === "close" &&
           lastDisconnect &&
@@ -107,24 +160,34 @@ router.get("/", async (req, res) => {
           lastDisconnect.error.output.statusCode !== 401
         ) {
           await delay(10000);
+
           RobinPair();
         }
       });
+
     } catch (err) {
       exec("pm2 restart Robin-md");
+
       console.log("service restarted");
+
       RobinPair();
+
       await removeFile("./session");
+
       if (!res.headersSent) {
-        await res.send({ code: "Service Unavailable" });
+        await res.send({
+          code: "Service Unavailable",
+        });
       }
     }
   }
+
   return await RobinPair();
 });
 
 process.on("uncaughtException", function (err) {
   console.log("Caught exception: " + err);
+
   exec("pm2 restart Robin");
 });
 
